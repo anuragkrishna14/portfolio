17a1979b9de90935a12f438be75122a4cdeb156a
Anurag Krishna <anuragkrishna14@gmail.com>
2025-02-18 15:56:26 +0530

    add waving animation

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
     <header>
       <div className="container header__container">
         <h5>Hello I'm</h5>
-        <h1>Anurag Krishna🖐️</h1>
+        <h1>Anurag Krishna<span className="wave">🖐️</span></h1>
         <h5 className="text-light">Fullstack Developer</h5>
         <CTA/>
         <HeaderSocials/>
